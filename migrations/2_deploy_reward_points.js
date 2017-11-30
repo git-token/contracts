42f5eabd7fe2e4148273bafb91c44cfceaad343a
Ryan Tate <ryan.michael.tate@gmail.com>
2017-11-29 20:38:12 -0800

    Redeployed RewardPoints & changes argument to decimals per consturctor parameters

--- migrations/2_deploy_reward_points.js
+++ migrations/2_deploy_reward_points.js
@@ -1,10 +1,10 @@
 const RewardPoints = artifacts.require('./RewardPoints.sol');
-const { admin } = require('../gittoken.config.js')
+const { decimals } = require('../gittoken.config.js')
 
 module.exports = function(deployer) {
   deployer.deploy(
     RewardPoints,
-    admin
+    decimals
   ).then((contract) => {
     console.log('contract', contract)
   }).catch((error) => {
